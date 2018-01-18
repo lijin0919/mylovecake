94c5523015e941e3054d692c80b2e0b23932c311
Administrator <317395616@qq.com>
2018-01-18 17:07:52 +0800

    重写了获取首页数据的service层方法                             ----刘康

--- src/main/java/com/controller/IndexController.java
+++ src/main/java/com/controller/IndexController.java
@@ -1,9 +1,6 @@
 package com.controller;
 
 import com.entity.Top;
-import com.entity.Types;
-import com.mapper.IIndexMapper;
-import com.mapper.ITopMapper;
 import com.service.ITopService;
 import com.service.TopServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import java.util.List;
 
 @Controller
-@SessionAttributes("typeList")
 public class IndexController {
 
 
@@ -23,12 +19,20 @@ public class IndexController {
    private TopServiceImpl topService;
 
     @GetMapping("/index")
-    public String getTypes(Model model,Model model1){
+    public String getTypes(Model model,Model model1,Model model2){
 
         try {
-            //进行查询
-            List<Top> topList = topService.getTopList();
-            model.addAttribute("topList",topList);
+//            //进行查询
+//            List<Top> topList = topService.getTopList();
+//            model.addAttribute("topList",topList);
+            //根据typeId查询
+            List<Top> topList1 = topService.getTopListByTypeId(1);
+            model.addAttribute("topList1",topList1);
+            List<Top> topList2 = topService.getTopListByTypeId(2);
+            model1.addAttribute("topList2",topList2);
+            List<Top> topList3 = topService.getTopListByTypeId(3);
+            model2.addAttribute("topList3",topList3);
+//            System.out.println(topList1);
 
         }catch (Exception e){
             e.printStackTrace();

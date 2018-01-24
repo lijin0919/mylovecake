f3400c915e99d7afe8feac78d253fb13649c90c5
677jiayou <444718843@qq.com>
2018-01-24 13:05:39 +0800

    实现订单中心展示 -----马迪

--- src/main/webapp/js/order.js
+++ src/main/webapp/js/order.js
@@ -41,37 +41,3 @@ function orderTopay(orderId) {
 function deleteOrder(orderId) {
     $("#order" + orderId).remove();
 }
-
-// $().ready(function () {
-//     $.ajax({
-//         url: "orderAction",
-//         type: "POST",
-//         dataType: "Json",
-//         success: function (respons) {
-//             for(var i=0;i<respons.length;i++){
-//               if(respons[i].status==1){
-//                   alert("1");
-//                   $(".btn-receive"+respons[i].id).attr({"disabled":"disabled"});
-//               }
-//               if(respons[i].status==2){
-//                   alert("2");
-//
-//                   $(".btn-topay" + orderId).text("已付款");
-//                   $(".btn-topay"+respons[i].id).attr({"disabled":"disabled"});
-//               }
-//                 if(respons[i].status==3){
-//                     alert("3");
-//
-//                     $(".btn-topay" + orderId).text("已付款");
-//                     $(".btn-receive" + orderId).text("已完成");
-//                     $(".btn-topay"+respons[i].id).attr({"disabled":"disabled"});
-//                     $(".btn-receive"+respons[i].id).attr({"disabled":"disabled"});
-//                 }
-//
-//             }
-//         },
-//         error: function () {
-//             alert("请求失败乐乐乐乐乐乐乐");
-//         }
-//     })
-// });
